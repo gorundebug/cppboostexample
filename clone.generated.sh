63676e90f906eae0dbbff548476844d646f7ec14
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-08-24 05:06:04 +0300

    Add Temporal automation service example

--- clone.generated.sh
+++ clone.generated.sh
@@ -20,6 +20,7 @@ clone_if_missing() {
 
 echo "==> Cloning services..."
 clone_if_missing "analyticsservice" "https://github.com/gorundebug/cppboostexample-analyticsservice.git" "v0.2.12"
+clone_if_missing "automationservice" "https://github.com/gorundebug/cppboostexample-automationservice.git" "v0.2.12"
 clone_if_missing "inventoryservice" "https://github.com/gorundebug/cppboostexample-inventoryservice.git" "v0.2.12"
 clone_if_missing "orderservice" "https://github.com/gorundebug/cppboostexample-orderservice.git" "v0.2.12"
 
