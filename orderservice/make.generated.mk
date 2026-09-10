67e3aae9ece6a6cd0200115d372dad300f5dd4b2
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-09-10 23:40:44 +0300

    Regenerate for framework release v0.2.115

--- orderservice/make.generated.mk
+++ orderservice/make.generated.mk
@@ -25,7 +25,7 @@ export MODULE_ORDER_SERVICE_API_SOURCE_CONTEXT := ../order_service_api
 endif
 
 ifneq ($(strip $(DEPENDENCY_PROXY_DIR)),)
-export SERVICELIB_SOURCE_CONTEXT ?= $(DEPENDENCY_GIT_MIRROR_DOCKER_BASE)/github.com/gorundebug/cppboostservicelib.git\#v0.2.114
+export SERVICELIB_SOURCE_CONTEXT ?= $(DEPENDENCY_GIT_MIRROR_DOCKER_BASE)/github.com/gorundebug/cppboostservicelib.git\#v0.2.115
 endif
 
 .PHONY: cpp-tools build test release-build release-test asan-build asan-start asan-up asan-down asan-test \
